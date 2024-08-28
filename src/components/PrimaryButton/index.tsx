95cc330c0c0f5c1d419e7b71c366730f9e679ef5
dali1999 <dali1999@naver.com>
2024-08-28 20:29:30 +0900

    feat: primary button 공통 컴포넌트 추가

---
+++ src/components/PrimaryButton/index.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react';
+import { device } from '@styles/breakpoints';
+import styled from 'styled-components';
+
+interface TPrimaryButtonProps {
+  children: ReactNode;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const PrimaryButton = ({ children, disabled, onClick }: TPrimaryButtonProps) => {
+  return (
+    <S.Container $isDisabled={disabled as boolean} onClick={onClick}>
+      {children}
+    </S.Container>
+  );
+};
+
+export default PrimaryButton;
+
+const S = {
+  Container: styled.button<{ $isDisabled: boolean }>`
+    background-color: ${({ $isDisabled }) => ($isDisabled ? '#B5CFB0' : '#6c9460')};
+    cursor: ${({ $isDisabled }) => ($isDisabled ? 'normal' : 'pointer')};
+    color: white;
+    text-align: center;
+    width: 100%;
+    line-height: 3rem;
+    font-size: 2rem;
+    padding: 1.7rem 0;
+    border-radius: 0.6rem;
+    @media ${device.mobile} {
+      font-size: 1.6rem;
+      padding: 1.4rem 0;
+    }
+  `,
+};
