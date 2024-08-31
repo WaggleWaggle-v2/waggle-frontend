a72d9cb714ba0281957fec5201735c31e102fb68
codingaring <sksms2104@naver.com>
2024-08-31 15:58:30 +0900

    feat:` /setup` 페이지에서 반응형 헤더 다르게 적용

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
-import Nav from './nav';
-import { device } from '@styles/breakpoints';
+import { device, size } from '@styles/breakpoints';
 import MainLogo from '@/assets/images/symbol-logo.png';
+import goBackIcon from '@assets/icons/left-arrow.svg';
+import Nav from './nav';
 
 const Header = () => {
+  const [pageWidth, setPageWidth] = useState(window.innerWidth);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newPageWidth = window.innerWidth;
+      if (newPageWidth !== pageWidth) setPageWidth(newPageWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <S.Container>
-      <S.MainLogo src={MainLogo} alt="메인 로고" />
-      <Nav isLogin={true} nickName={'홍길동동동동'} />
+      {pathname === '/setup' && pageWidth <= size.tablet ? (
+        <S.GoBackIcon src={goBackIcon} alt="뒤로 가기" />
+      ) : (
+        <>
+          <S.MainLogo src={MainLogo} alt="메인 로고" />
+          <Nav isLogin={true} nickName={'홍길동동동동'} />
+        </>
+      )}
     </S.Container>
   );
 };
@@ -24,6 +48,7 @@ const S = {
     z-index: 20;
 
     @media ${device.tablet} {
+      align-items: center;
     }
   `,
 
@@ -31,5 +56,15 @@ const S = {
     width: 16.2rem;
     object-fit: contain;
     margin-left: 1rem;
+
+    @media ${device.tablet} {
+    }
+  `,
+
+  GoBackIcon: styled.img`
+    width: 1rem;
+    height: 2rem;
+    margin-left: 2rem;
+    cursor: pointer;
   `,
 };
