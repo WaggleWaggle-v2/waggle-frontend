b095d227cb18536472ecf6bb9b95c45206c7e1fb
dali1999 <dali1999@naver.com>
2024-08-31 16:23:23 +0900

    feat: 로그인 페이지 나오지 않는 페이지 Root에서 관리

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,17 @@
 import Header from '@components/header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Root = () => {
+  const location = useLocation();
+
+  const nonSubSectionArray = ['/login'];
+  const isSubSection = !nonSubSectionArray.includes(location.pathname);
+
   return (
     <S.Container>
-      <Header />
+      {isSubSection && <Header />}
+
       <S.Main>
         <Outlet />
       </S.Main>
@@ -18,7 +24,7 @@ export default Root;
 const S = {
   Container: styled.div`
     background-color: #f6f4ee;
-    height: 100vh;
+    height: 100%;
     display: grid;
     grid-template-rows: 9.6rem 1fr;
     grid-template-areas:
