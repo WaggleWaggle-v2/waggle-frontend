ecb9b622ccffef052e846d4662c3bf8a71f40222
codingaring <sksms2104@naver.com>
2024-09-10 01:09:43 +0900

    feat: scroller 안내 아이콘 추가

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -7,11 +7,18 @@ import treeImg from '@assets/images/tree.png';
 import houseImg from '@assets/images/house.png';
 import BookshelfSection from './components/BookshelfSection';
 import typography from '@assets/images/typography-short.png';
+import { scrollerAnimation } from './style/scrollAnimatioin';
 
 const Landing = () => {
   const navigate = useNavigate();
   return (
     <>
+      <S.ScrollContainer>
+        <S.Mouse>
+          <S.Scroller />
+        </S.Mouse>
+        <S.ScrollText>Scroll</S.ScrollText>
+      </S.ScrollContainer>
       <S.Main>
         <S.LandingTitleBox>
           <S.LogoIcon src={mainLogo} alt={'와글와글'} />
@@ -156,6 +163,12 @@ const S = {
     line-height: 170%;
     font-weight: bold;
   `,
+  ScrollText: styled.p`
+    display: block;
+    margin-top: 0.8rem;
+    text-align: center;
+    color: var(--button-active);
+  `,
 
   //Element Style
   LogoIcon: styled.img`
@@ -175,5 +188,30 @@ const S = {
     &:active {
       background-color: var(--green700);
     }
+  `,
+  ScrollContainer: styled.div`
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    bottom: 2%;
+    right: 2%;
+    transform: translate(0, -35%);
+    z-index: 30;
+  `,
+  Mouse: styled.div`
+    width: 0.3rem;
+    padding: 0.8rem 1.2rem;
+    height: 4rem;
+    border: 0.2rem solid var(--button-active);
+    border-radius: 2.5rem;
+    opacity: 0.75;
+  `,
+  Scroller: styled.div`
+    width: 0.3rem;
+    height: 1rem;
+    border-radius: 25%;
+    background-color: var(--button-active);
+    animation: ${scrollerAnimation} 2.25s cubic-bezier(0.15, 0.41, 0.69, 0.94) infinite;
   `,
 };
