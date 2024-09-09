652582def2621fb7685c79b5b15bda9e4dc2e7c5
yu kyoung kim <sksms2104@naver.com>
2024-09-09 11:54:06 +0900

    feat: setup 페이지 헤더 구현

--- src/pages/setup/index.tsx
+++ src/pages/setup/index.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import ProgressBar from '@components/template/ProgressBar';
 import SettingTemplate from '@components/template/SettingTemplate';
 import { USER_SETUP_TOTAL_STEP } from '@constants/setupTotalStep';
-import { device } from '@styles/breakpoints';
+import { device, size } from '@styles/breakpoints';
 import { HEADER_HEIGHT } from '@styles/headerHeight';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import SetNickname from './components/SetNickname';
 import SetPublicity from './components/SetPublicity';
 import { TPublicity } from './types/type';
+import Header from '@components/header';
+import usePageWidth from '@hooks/usePageWidth';
+import backIcon from '@assets/icons/left-arrow.svg';
 
 const SetUp = () => {
   const [step, setStep] = useState(1);
@@ -16,6 +19,7 @@ const SetUp = () => {
   const [nickname, setNickname] = useState('');
   const [publicity, setPublicity] = useState<TPublicity>(null);
   const navigate = useNavigate();
+  const pageWidth = usePageWidth();
 
   const handleNextButtonClick = () => {
     setStep(step => step + 1);
@@ -26,43 +30,67 @@ const SetUp = () => {
   };
 
   return (
-    <S.Container>
-      {step === 1 && (
-        <SettingTemplate
-          step={step}
-          titleTop="호패를 만드시오"
-          handleButtonClick={handleNextButtonClick}
-          isDisabled={isDisabled}>
-          <SetNickname setNickname={setNickname} setIsDisabled={setIsDisabled} />
-        </SettingTemplate>
+    <>
+      {pageWidth > size.tablet ? (
+        <Header />
+      ) : (
+        <S.HeaderContainer>
+          <S.GoBackIcon
+            src={backIcon}
+            alt={'뒤로 가기'}
+            onClick={() => (step !== 1 ? setStep(prev => prev - 1) : navigate('/login'))}
+          />
+        </S.HeaderContainer>
       )}
-
-      {step === 2 && (
-        <SettingTemplate
-          step={step}
-          titleTop="공개 하겠소?"
-          handleButtonClick={handleSubmitButtonClick}
-          isDisabled={isDisabled}>
-          <SetPublicity setPublicity={setPublicity} publicity={publicity} />
-        </SettingTemplate>
-      )}
-      <ProgressBar totalStep={USER_SETUP_TOTAL_STEP} currentStep={step} />
-    </S.Container>
+      <S.Body>
+        {step === 1 && (
+          <SettingTemplate
+            step={step}
+            titleTop="호패를 만드시오"
+            handleButtonClick={handleNextButtonClick}
+            isDisabled={isDisabled}>
+            <SetNickname setNickname={setNickname} setIsDisabled={setIsDisabled} />
+          </SettingTemplate>
+        )}
+        {step === 2 && (
+          <SettingTemplate
+            step={step}
+            titleTop="공개 하겠소?"
+            handleButtonClick={handleSubmitButtonClick}
+            isDisabled={isDisabled}>
+            <SetPublicity setPublicity={setPublicity} publicity={publicity} />
+          </SettingTemplate>
+        )}
+        <ProgressBar totalStep={USER_SETUP_TOTAL_STEP} currentStep={step} />
+      </S.Body>
+    </>
   );
 };
 
 export default SetUp;
 const S = {
-  Container: styled.div`
+  HeaderContainer: styled.header`
+    position: sticky;
+    top: 0;
+    padding: 2rem;
+    display: flex;
+    align-items: center;
+    height: ${HEADER_HEIGHT.MOBILE};
+  `,
+  Body: styled.main`
     display: flex;
     align-items: center;
     justify-content: center;
-    height: calc(100vh);
+    height: 100vh;
+
     @media ${device.tablet} {
-      height: calc(100vh - 10.2rem);
+      height: calc(100vh - ${HEADER_HEIGHT.MOBILE});
     }
     @media ${device.mobile} {
-      height: calc(76vh - ${HEADER_HEIGHT.MOBILE} - 10.2rem);
+      height: calc(76vh - ${HEADER_HEIGHT.MOBILE});
     }
+  `,
+  GoBackIcon: styled.img`
+    cursor: pointer;
   `,
 };
