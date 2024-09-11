602df3a50953be56fe675fd736bd948d3d8bb820
codingaring <sksms2104@naver.com>
2024-09-11 20:21:18 +0900

    feat: landing Button 수정

--- src/pages/landing/components/ButtonSection/components/LandingButton.tsx
+++ src/pages/landing/components/ButtonSection/components/LandingButton.tsx
@@ -12,68 +12,73 @@ type TType = 'green' | 'beige';
 
 const LandingButton = ({ icon, children, type, fontSize }: TLandingButton) => {
   return (
-    <S.ButtonBox $type={type}>
-      <S.ButtonTextBox $fontSize={fontSize} $type={type} className="text-box">
-        <S.ButtonIcon src={icon} />
-        {children}
-      </S.ButtonTextBox>
-    </S.ButtonBox>
+    <S.Wrapper $type={type}>
+      <S.Button $type={type} className="button-box">
+        <S.SubBox $type={type} $fontSize={fontSize} className="button-content">
+          <S.ButtonIcon src={icon} />
+          {children}
+        </S.SubBox>
+      </S.Button>
+    </S.Wrapper>
   );
 };
 
 export default LandingButton;
 
 const S = {
-  ButtonBox: styled.button<{ $type: TType }>`
-    display: grid;
-    grid-template-columns: 4.2rem 1fr;
-    grid-template-rows: 14rem 2.3rem;
+  Wrapper: styled.div<{ $type: TType }>`
+    position: relative;
     width: 100%;
-    background-color: ${({ $type }) => ($type === 'green' ? 'var(--green600)' : '#e8dfcf')};
-    cursor: pointer;
+    aspect-ratio: 2 / 1;
     border: 0.1rem solid ${({ $type }) => ($type === 'green' ? 'var(--white)' : 'var(--green600)')};
-    transition: background-color 0.3s ease-in-out;
-
-    &:hover .text-box {
-      color: var(--white);
-      border-color: var(--white);
-    }
-
-    &:hover {
-      background-color: var(--green600);
-      transition: background-color 0.3s ease-in-out;
-      border-color: var(--white);
-    }
-
-    &::before {
-      content: '';
-
-      clip-path: polygon(0 0, 100% 0%, 100% 86%, 0% 100%);
-      top: 0;
-      left: 0;
-      grid-row-start: 1;
-      grid-row-end: 3;
-      background-color: red;
-    }
+    cursor: pointer;
 
     &::after {
       content: '';
-      margin-left: -4.2rem;
-      clip-path: polygon(14% 0, 100% 0%, 100% 100%, 0 100%);
-      top: 0;
+      position: absolute;
+      bottom: 0;
       left: 0;
-      grid-row-start: 2;
-      grid-row-end: 3;
-      background-color: blue;
+      width: 100%;
+      height: 1px;
+      background-color: ${({ $type }) => ($type === 'green' ? 'var(--white)' : 'var(--green600)')};
+      transform: rotate(-31deg);
+      transform-origin: bottom left;
+      z-index: 0;
+    }
+
+    &:hover .button-content {
+      background-color: ${({ $type }) => ($type === 'green' ? 'var(--green800)' : 'var(--green200)')};
+      transition: background-color 0.2s ease-in-out;
+    }
+
+    &:hover .button-box {
+      background-color: ${({ $type }) => ($type === 'green' ? 'var(--green800)' : 'var(--green200)')};
+      transition: background-color 0.2s ease-in-out;
     }
   `,
-  ButtonTextBox: styled.div<{ $fontSize?: string; $type: TType }>`
-    border: 0.1rem solid ${({ $type }) => ($type === 'green' ? 'var(--white)' : 'var(--green600)')};
+  Button: styled.div<{ $type: TType }>`
+    aspect-ratio: 2 / 1;
+    background-color: ${({ $type }) => ($type === 'green' ? 'var(--green600)' : '#E8DFCF')};
+    position: relative;
+    transition: background-color 0.3s ease-in-out;
+  `,
+
+  SubBox: styled.div<{ $type: TType; $fontSize?: string }>`
+    width: 27rem;
+    height: 13rem;
+    background-color: ${({ $type }) => ($type === 'green' ? 'var(--green600)' : '#E8DFCF')};
+    border-bottom: 1px solid ${({ $type }) => ($type === 'green' ? 'var(--white)' : 'var(--green600)')};
+    border-left: 0.1rem solid ${({ $type }) => ($type === 'green' ? 'var(--white)' : 'var(--green600)')};
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 1;
     padding: 1.5rem 1.8rem;
     color: ${({ $type }) => ($type === 'green' ? 'var(--white)' : 'var(--green600)')};
     font-family: 'EBSHMJESaeron';
     font-size: ${({ $fontSize }) => ($fontSize ? $fontSize : '3.2rem')};
     line-height: 130%;
+    transition: background-color 0.2s ease-in-out;
   `,
   ButtonIcon: styled.img`
     width: 2.2rem;
