f14ff6062812a50823d254e9c267de58d5f7ea8e
dali1999 <dali1999@naver.com>
2024-10-02 02:09:55 +0900

    feat: 텍스트 입력 시 숫자는 허용하도록 수정

--- src/pages/setup/utils/isCompleteKoreanWord.ts
+++ src/pages/setup/utils/isCompleteKoreanWord.ts
@@ -1,5 +1,5 @@
 export const isCompleteKoreanWord = (text: string) => {
-  const koreanRegex = /^[\uAC00-\uD7A3\u1100-\u11FF\u3130-\u318F\s\p{P}\p{S}]+$/u;
+  const koreanRegex = /^[\uAC00-\uD7A3\u1100-\u11FF\u3130-\u318F\s\p{P}\p{S}\d]+$/u;
   const nonKoreanRegex = /[a-zA-Z]/;
 
   if (nonKoreanRegex.test(text)) {
